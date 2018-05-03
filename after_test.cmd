5908519c829e0cb18d9f37e90a6561bd110b172e
Gobbenobber <gobbenobber@gmail.com>
2018-05-03 16:14:02 +0200

    Maybe quotation marks are needed.

--- after_test.cmd
+++ after_test.cmd
@@ -4,6 +4,6 @@ nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 cd tools
 REM nunit3-console C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\bin\Debug\netcoreapp2.0\WebserverUnitTests.dll --result=myresults.xml;format=AppVeyor
 REM OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -oldStyle -register:user -targetargs:"test C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --test-adapter-path:. --logger:Appveyor" -filter:"+[*]* -[*.Tests]*"
-dotnet test C:\projects\swagattack\SwagAttack_Gameserver_Mirror\GameServer\ITTestCore\IT_Core.csproj --no-build --no-restore --logger:Appveyor /p:Exclude=D:/repos/nunit/nunit3-vs-adapter/src/NUnitTestAdapter/*.cs /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test C:\projects\swagattack\SwagAttack_Gameserver_Mirror\GameServer\ITTestCore\IT_Core.csproj --no-build --no-restore --logger:Appveyor /p:Exclude="D:/repos/nunit/nunit3-vs-adapter/src/NUnitTestAdapter/*.cs" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i ..\SwagAttack_Gameserver_Mirror\GameServer\ITTestCore\coverage.xml --jobId %APPVEYOR_BUILD_NUMBER%
 REM C:\projects\swagattack-gui-webserver-mirror\GUI\GUI_Index.unit.test\WebserverUnitTests.csproj --no-build --no-restore
